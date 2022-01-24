9ee28b871e2646bc82f975b7df6a9b0e5a12d999
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-01-24 15:16:05 -0600

    renamed parameters and notebooks

--- scripts/run_puncta_analysis.py
+++ scripts/run_puncta_analysis.py
@@ -3,7 +3,7 @@ import os
 
 from punctatools.lib.quantify import quantify_batch
 from punctatools.lib.segment import segment_puncta_batch
-from punctatools.lib.utils import load_parameters
+from punctatools.lib.utils import load_parameters, save_parameters
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,10 +13,10 @@ if __name__ == '__main__':
 
     param_keys = [
         'puncta_segm_dir',
-        'puncta_stat_dir',
-        'cell_stat_dir',
+        'puncta_quant_dir',
+        'roi_quant_dir',
         'puncta_channels',
-        'cell_segmentation',
+        'roi_segmentation',
         'minsize_um',
         'maxsize_um',
         'num_sigma',
@@ -28,18 +28,21 @@ if __name__ == '__main__':
         'background_percentile',
         'threshold_segmentation',
         'segmentation_mode',
-        'remove_out_of_cell',
+        'remove_out_of_roi',
         'maxrad_um',
         'n_jobs',
         'channel_names'
     ]
     param_matches = dict(output_dir='puncta_analysis_dir')
     kwargs = load_parameters(vars(), param_keys, param_matches)
-    if kwargs['cell_segmentation']:
-        param_matches['input_dir'] = 'cell_segmentation_dir'
+    if kwargs['roi_segmentation']:
+        param_matches['input_dir'] = 'roi_segmentation_dir'
     else:
         param_matches['input_dir'] = 'converted_data_dir'
     kwargs = load_parameters(vars(), param_keys, param_matches)
+    os.makedirs(kwargs['output_dir'], exist_ok=True)
+    params = save_parameters(kwargs,
+                             os.path.join(kwargs['output_dir'], parameter_file.split('/')[-1]))
 
     print('\nThe following are the parameters that will be used:')
     print(kwargs)
@@ -47,8 +50,8 @@ if __name__ == '__main__':
 
     channel_names = kwargs.pop('channel_names')
     puncta_segm_dir = kwargs.pop('puncta_segm_dir')
-    cell_stat_dir = kwargs.pop('cell_stat_dir')
-    puncta_stat_dir = kwargs.pop('puncta_stat_dir')
+    roi_quant_dir = kwargs.pop('roi_quant_dir')
+    puncta_quant_dir = kwargs.pop('puncta_quant_dir')
 
     segm_kwargs = kwargs.copy()
     segm_kwargs['output_dir'] = os.path.join(segm_kwargs['output_dir'], puncta_segm_dir)
@@ -57,8 +60,8 @@ if __name__ == '__main__':
     print('\n')
 
     quantify_batch(input_dir=segm_kwargs['output_dir'],
-                   output_dir_puncta=os.path.join(kwargs['output_dir'], puncta_stat_dir),
-                   output_dir_roi=os.path.join(kwargs['output_dir'], cell_stat_dir),
+                   output_dir_puncta=os.path.join(kwargs['output_dir'], puncta_quant_dir),
+                   output_dir_roi=os.path.join(kwargs['output_dir'], roi_quant_dir),
                    parallel=True, n_jobs=kwargs['n_jobs'],
                    channel_names=channel_names,
                    puncta_channels=kwargs['puncta_channels'],
