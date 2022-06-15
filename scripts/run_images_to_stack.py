978f6aef7814e7645947436da42bc904f5d48cd6
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-06-15 15:58:28 -0500

    separate parameters and paths; separate parameters for different steps

--- scripts/run_images_to_stack.py
+++ scripts/run_images_to_stack.py
@@ -1,20 +1,30 @@
 import argparse
+import json
 
 from punctatools.lib.convert import images_to_stacks
-from punctatools.lib.utils import load_parameters
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--parameter-file', type=str, help='json file with parameters', required=True)
+    parser.add_argument('-j', '--n-jobs', type=int, help='number of processes to run in parallel', default=8)
+    parser.add_argument('-i', '--input-dir', type=str, default=None,
+                        help='folder with images to be converted')
+    parser.add_argument('-o', '--output-dir', type=str, default=None,
+                        help='folder to save results')
     args = parser.parse_args()
     parameter_file = args.parameter_file
 
-    param_keys = ['channel_code', 'z_position_code', 'spacing', 'n_jobs']
-    param_matches = dict(input_dir='raw_dir', output_dir='converted_data_dir')
-    kwargs = load_parameters(vars(), param_keys, param_matches)
+    with open(parameter_file) as f:
+        params = json.load(f)
 
-    print('\nThe following are the parameters that will be used:')
-    print(kwargs)
+    params['n_jobs'] = args.n_jobs
+    if args.input_dir is not None:
+        params['input_dir'] = args.input_dir.rstrip('/')
+    if args.output_dir is not None:
+        params['output_dir'] = args.output_dir.rstrip('/')
+
+    print('\nThe following parameters will be used for conversion:')
+    print(params)
     print('\n')
 
-    images_to_stacks(parallel=True, process_name='Convert images to stacks', **kwargs)
+    images_to_stacks(parallel=True, process_name='Convert images to stacks', **params)
